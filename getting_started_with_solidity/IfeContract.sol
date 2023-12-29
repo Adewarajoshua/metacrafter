5d4ffbd92a003b96db3efd390195791e8c253fdb
Adewarajoshua <155187870+Adewarajoshua@users.noreply.github.com>
2023-12-29 19:40:25 +0100

    Create IfeContract.sol

---
+++ getting_started_with_solidity/IfeContract.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.22;
+
+/**
+ * @title IfeContract
+ * @dev A simple token contract demonstrating minting and burning functionalities.
+ */
+contract IfeContract {
+
+    // Public variables
+    string public tokenName = 'IfeToken';
+    string public tokenSymbol = 'IFT';
+    uint public totalSupply = 0;
+
+    // Mapping variable
+    mapping(address => uint) public balance;
+
+    /**
+     * @dev Burns tokens, reducing the total supply and sender's balance.
+     * @param _value The amount of tokens to burn.
+     * @param _address The address of the sender.
+     */
+    function burn(uint _value, address _address) public {
+        require(balance[_address] >= _value, "Insufficient balance");
+        totalSupply -= _value;
+        balance[_address] -= _value;
+    }
+
+    /**
+     * @dev Mints new tokens, increasing the total supply and sender's balance.
+     * @param _value The amount of tokens to mint.
+     * @param _address The address of the sender.
+     */
+    function mint(uint _value, address _address) public {
+        totalSupply += _value;
+        balance[_address] += _value;
+    }
+}
